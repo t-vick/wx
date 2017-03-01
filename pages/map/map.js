3e616d87ea6d3ae9a8116b016ea64ec70d3c4549
t-vick <t-vick@msn.com>
2017-03-01 18:02:12 +0800

    最小的车库单元已画好

--- pages/map/map.js
+++ pages/map/map.js
@@ -23,34 +23,91 @@ Page({
         height: 10
       }
     ],
+    cylinderList: [
+      {
+        x: 50,
+        y: 270,
+        r: 5
+      },
+      {
+        x: 310,
+        y: 270,
+        r: 5
+      },
+      {
+        x: 50,
+        y: 380,
+        r: 5
+      },
+      {
+        x: 310,
+        y: 380,
+        r: 5
+      }
+    ],
       pathList: [
       {
         startX: -10,
         endX: 400,
         startY: 250,
         endY: 250,
-        width: 30
+        width: 20
       },
        {
         startX: -10,
         endX: 400,
         startY: 400,
         endY: 400,
-        width: 30
+        width: 20
       },
        {
         startX: 30,
         endX: 30,
         startY: -10,
         endY: 800,
-        width: 30
+        width: 20
       },
        {
         startX: 330,
         endX: 330,
         startY: -10,
         endY: 800,
-        width: 30
+        width: 20
+      }
+    ],
+    cellList: [
+      {
+        x: 60,
+        y: 275,
+        w: 78,
+        h: 100,
+        num: {
+          x: 80,
+          y: 300,
+          text: '301'
+        }
+      },
+      {
+        x: 140,
+        y: 275,
+        w: 78,
+        h: 100,
+        num: {
+          x: 160,
+          y: 300,
+          text: '302',
+        }
+      },
+      {
+        x: 220,
+        y: 275,
+        w: 78,
+        h: 100,
+        num: {
+          x: 240,
+          y: 300,
+          text: '303'
+        }
       }
     ]
     }
--- utils/map.js
+++ utils/map.js
@@ -3,7 +3,7 @@ class Map {
         this.ctx = wx.createCanvasContext(canvasId);
         this.ctx.setLineCap('round');
         this.ctx.setLineJoin('round');
-        this.ctx.setGlobalAlpha(0.2);
+        this.ctx.setGlobalAlpha(0.3);
     }
 
     setOption(option) {
@@ -14,6 +14,8 @@ class Map {
                 _this.ctx.fillRect(-option.xLoc,-option.yLoc,data.windowWidth,data.windowHeight);
                 _this.drawPillars(option.pillarList);
                 _this.drawPaths(option.pathList);
+                _this.drawCylinders(option.cylinderList);
+                _this.drawCells(option.cellList);
                 _this.ctx.draw();
             },
             fail: function(err) {
@@ -32,6 +34,16 @@ class Map {
         }
     }
 
+    drawCylinders(cylinderList) {
+        // this.ctx.beginPath();
+        this.ctx.setFillStyle('green');
+        for (let cylinder of cylinderList) {
+            this.ctx.beginPath();
+            this.ctx.arc(cylinder.x,cylinder.y,cylinder.r,0,2 * Math.PI);
+            this.ctx.fill();
+        }
+    }
+
     drawPaths (pathList) {
         this.ctx.setStrokeStyle('blue');
         // this.ctx.beginPath();
@@ -42,6 +54,22 @@ class Map {
             // this.ctx.stroke();//这句代码不能放在这里，否则线条的透明不不一致，
         }
         this.ctx.stroke();//放在这里才能保证画出的线条的透明度一致
+    }
+
+    drawCells (cellList) {
+        this.ctx.setFillStyle('green');
+        this.ctx.setFontSize(30);
+        for (let cell of cellList) {
+            this.ctx.setFillStyle('green');
+            this.ctx.fillRect(cell.x, cell.y, cell.w, cell.h);
+            this.ctx.setFillStyle('black');
+            this.ctx.fillText(cell.num.text, cell.num.x, cell.num.y);
+            // this.ctx.setFillStyle('white');
+            // for (let i=1; i<cell.lineCount;i++) {
+            //     this.ctx.fillRect(cell.x + cell.spotWidth * i + cell.lineWidth * (i -1),cell.y,cell.lineWidth, cell.h);
+            //     // this.ctx.lineTo(cell.x + cell.spotWidth * i,cell.y + cell.h);
+            // }
+        }
     }
 
     translate(x, y) {
